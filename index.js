0d68c429d363b1b65ee52476df5361fe03d3fe1f
Jennifer Schelkopf <jenschelkopf@github.com>
2020-03-27 09:55:01 -0700

    Add recipient input

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const github = require('@actions/github');
 try {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  const labelRecipients = core.getInput('recipients').split("\n");
+
+  labelRecipients.forEach(labelRecipient => {
+    const mapping = labelRecipient.split("=");
+    console.log(`When the ${mapping[0]} label is added to an issue notify ${mapping[1]}`);
+  })
+
 } catch (error) {
   core.setFailed(error.message);
 }
